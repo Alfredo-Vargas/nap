929581e7cf583d3b2b8944158c4b12ebd59536c5
Alfredo Vargas <alfredo.vargas-paredes@outlook.com>
2021-11-14 15:59:43 +0100

    First test wih pyshark

--- nap.py
+++ nap.py
@@ -1,5 +1,14 @@
 import pyshark
 
-print("Hello World")
+allPackets = pyshark.FileCapture("pcap/network-traffic.pcap")
 
-c = pyshark.FileCapture("pcap/network-traffic.pcap")
+pkt1 = allPackets[0]
+
+print("IP" in pkt1)
+ # pkt1.layers
+
+print(pkt1.ip.version)
+#pkt1[0]
+
+#pkt1.ip.field_names
+

c06a6d40baea31f56c6dfe7aabf7c1868e58d06f
Alfredo Vargas-Paredes <alfredo.vargas-paredes@outlook.com>
2021-12-13 17:31:08 +0100

    Fixing lint anti-patterns

--- NapMethods.py
+++ NapMethods.py
@@ -1,50 +1,50 @@
 import scapy.all as scapy
 import graphviz
-import numpy 
+import numpy
+
 
 def generate_conversations(pcap_file):
     # We read the pcap file
     all_traffic = scapy.rdpcap(pcap_file)
-    packets, frames = [] , []					# to store layer3 and layer2 traffic
+    packets, frames = [], []					# to store layer3 and layer2 traffic
     # We create a 2D array with Source+Destination and packet/frame length
     for item in all_traffic:
         if (scapy.IP in item):
             packets.append([item['IP'].src + ',' + item['IP'].dst, len(item)])
-        else:
-            if (scapy.Ether in item):
-                frames.append([item['Ether'].src + ',' + item['Ether'].dst, len(item)])
+        elif (scapy.Ether in item):
+            frames.append([item['Ether'].src + ',' + item['Ether'].dst, len(item)])
     packets = numpy.array(packets)
     frames = numpy.array(frames)
-    conversation_packets = list(set(packets[:,0]))
-    conversation_frames = list(set(frames[:,0]))
+    conversation_packets = list(set(packets[:, 0]))
+    conversation_frames = list(set(frames[:, 0]))
     packet_conv_matrix = numpy.transpose(numpy.array([conversation_packets, [0] * len(conversation_packets)]))
     frame_conv_matrix = numpy.transpose(numpy.array([conversation_frames, [0] * len(conversation_frames)]))
     # We add up the total conversation size
     for i in range(len(conversation_packets)):
         size = 0
-        for j in range(len(packets[:,0])):
-            if (packet_conv_matrix[i,0] == packets[j,0]):
-                size += int(packets[j,1])
-        packet_conv_matrix[i,1] = size
+        for j in range(len(packets[:, 0])):
+            if (packet_conv_matrix[i, 0] == packets[j, 0]):
+                size += int(packets[j, 1])
+        packet_conv_matrix[i, 1] = size
     for i in range(len(conversation_frames)):
         size = 0
-        for j in range(len(frames[:,0])):
-            if (frame_conv_matrix[i,0] == frames[j,0]):
-                size += int(frames[j,1])
-        frame_conv_matrix[i,1] = size
+        for j in range(len(frames[:, 0])):
+            if (frame_conv_matrix[i, 0] == frames[j, 0]):
+                size += int(frames[j, 1])
+        frame_conv_matrix[i, 1] = size
     # We split the information in different lists
-    src_ips, dst_ips, siz_ips = [] , [] , []
-    for i in range (len(packet_conv_matrix)):
-        ips = packet_conv_matrix[i,0].split(",")
+    src_ips, dst_ips, siz_ips = [], [], []
+    for i in range(len(packet_conv_matrix)):
+        ips = packet_conv_matrix[i, 0].split(",")
         src_ips.append(ips[0])
         dst_ips.append(ips[1])
-        siz_ips.append(packet_conv_matrix[i,1])
-    src_mac, dst_mac, siz_mac = [] , [] , []
-    for i in range (len(frame_conv_matrix)):
-        macs = frame_conv_matrix[i,0].split(",")
+        siz_ips.append(packet_conv_matrix[i, 1])
+    src_mac, dst_mac, siz_mac = [], [], []
+    for i in range(len(frame_conv_matrix)):
+        macs = frame_conv_matrix[i, 0].split(",")
         src_mac.append(macs[0])
         dst_mac.append(macs[1])
-        siz_mac.append(frame_conv_matrix[i,1])
+        siz_mac.append(frame_conv_matrix[i, 1])
     # We create readable labels for all IP/MAC addresses (dictionaries)
     ip_values = list(set(src_ips + dst_ips))
     mac_values = list(set(src_mac + dst_mac))
@@ -52,22 +52,20 @@ def generate_conversations(pcap_file):
     for i in range(1, len(ip_values) + 1):
         prefix = "IP" + str(i)
         ips_labels.append(prefix)
-    ips_label_dict = dict(zip(ips_labels,ip_values))
+    ips_label_dict = dict(zip(ips_labels, ip_values))
     for i in range(1, len(mac_values) + 1):
         prefix = "MAC" + str(i)
         mac_labels.append(prefix)
     mac_label_dict = dict(zip(mac_labels, mac_values))
 
-    ##########################################################################################	
     #    We create the Direct Graphic for the IP conversation and Ethernet conversation      #
     #                              https://graphviz.org/                                     #
-    ##########################################################################################	
     # Start Linear Mapping to define edge thickness
     conversation_ip_size = list(map(int, siz_ips))
     conversation_mac_size = list(map(int, siz_mac))
     p, q = 1, 8
-    m1, n1 = 0, max(conversation_ip_size) 
-    m2, n2 = 0, max(conversation_mac_size) 
+    m1, n1 = 0, max(conversation_ip_size)
+    m2, n2 = 0, max(conversation_mac_size)
     b1, b2 = (q - p) / (n1 - m1), (q - p) / (n2 - m2)
     a1, a2 = p - b1 * m1, p - b2 * m2
     mapped_ip_conversation_size = [a1 + b1 * x for x in conversation_ip_size]
@@ -139,14 +137,14 @@ def generate_conversations(pcap_file):
     dot_ether4.graph_attr['size'] = '8!'
 
     for key, value in mac_label_dict.items():
-        value = value.replace(':','.')
+        value = value.replace(':', '.')
         dot_ether1.node(value, key, color="darkblue", fontcolor="darkblue")
         dot_ether2.node(value, key, color="darkblue", fontcolor="darkblue")
         dot_ether3.node(value, key, color="darkblue", fontcolor="darkblue")
         dot_ether4.node(value, key, color="darkblue", fontcolor="darkblue")
     for i in range(len(src_mac)):
-        src = src_mac[i].replace(':','.')
-        dst = dst_mac[i].replace(':','.')
+        src = src_mac[i].replace(':', '.')
+        dst = dst_mac[i].replace(':', '.')
         dot_ether1.edge(src, dst, penwidth=mapped_mac_conversation_size[i], label=siz_mac[i], color="darkblue", fontcolor="red", labeldistance="0")
         dot_ether2.edge(src, dst, penwidth=mapped_mac_conversation_size[i], label=siz_mac[i], color="darkblue", fontcolor="red", labeldistance="0")
         dot_ether3.edge(src, dst, penwidth=mapped_mac_conversation_size[i], label=siz_mac[i], color="darkblue", fontcolor="red", labeldistance="0")
@@ -160,5 +158,6 @@ def generate_conversations(pcap_file):
     dot_ether3.render('conversations/ethernet_conversation_neato.gv')
     dot_ether4.render('conversations/ethernet_conversation_circo.gv')
 
-# Uncomment to Debug
-generate_conversations("./pcap/network-traffic.pcap")
+
+#  Uncomment to Debug
+#  generate_conversations("./pcap/network-traffic.pcap")
--- nap.py
+++ nap.py
@@ -10,22 +10,22 @@ from PyQt5.QtSvg import QGraphicsSvgItem
 from PyQt5.QtWidgets import (
     QApplication, QLabel, QRadioButton, QWidget,
     QHBoxLayout, QVBoxLayout,
-    QLineEdit, QFileDialog, QPushButton, QCheckBox, QGraphicsScene, QGraphicsView)
+    QLineEdit, QFileDialog, QPushButton, QGraphicsScene, QGraphicsView)
+
 
 class NapMainWindow(QWidget):
 
-
-    def __init__(self):		# constructor
-        super().__init__()	# shoud inherit all from the class NapMainWindow
+    def __init__(self):  # constructor
+        super().__init__()  # shoud inherit all from the class NapMainWindow
         self.setWindowTitle("NAP: Network Analyzer with Python")
         napMode = "off"
-        self.napMode = napMode # possible values, ip, mac, ...)
+        self.napMode = napMode  # possible values, ip, mac, ...)
         #  Create an outer layout
         outerLayout = QVBoxLayout()
         #  Create a form layout for the file path field, load pcap button and capture live button
         topLayout = QHBoxLayout()
 
-        self.pathToFile = QLineEdit('', self)	# you want to modify this property that is why becomes a public attribute
+        self.pathToFile = QLineEdit('', self)  # you want to modify this property that is why becomes a public attribute
         topLayout.addWidget(self.pathToFile, 2)
 
         btnLoadPcapFile = QPushButton('Load PCAP File', self)
@@ -34,10 +34,10 @@ class NapMainWindow(QWidget):
 
         topLayout.addWidget(QPushButton("Capture-Live"), 1)
 
-        ## Create the body layout  (graphviz and options will be placed there)
+        # Create the body layout  (graphviz and options will be placed there)
         bodyLayout = QHBoxLayout()
 
-        ### Graphics layout
+        # Graphics layout
         graphicsLayout = QVBoxLayout()
 
         btnGenerateConversations = QPushButton('Generate Conversations', self)
@@ -48,11 +48,11 @@ class NapMainWindow(QWidget):
         self.view = QGraphicsView(self.scene)
         graphicsLayout.addWidget(self.view)
 
-        ### Legend layout
+        # Legend layout
         legendLayout = QVBoxLayout()
         legendLayout.addWidget(QLabel("Legend"))
 
-        ### Options layout
+        # Options layout
         optionsLayout = QVBoxLayout()
 
         btnShowIPConversation = QPushButton("IP\nConversation", self)
@@ -79,9 +79,9 @@ class NapMainWindow(QWidget):
         outerLayout.addLayout(topLayout)
         outerLayout.addLayout(bodyLayout)
 
-        self.setGeometry(500,500, 1600, 900)
+        self.setGeometry(500, 500, 1600, 900)
         self.setLayout(outerLayout)
-    
+
         # Here are the Method Calls (place it after all attributes have been declared)
         btnShowEthernetConversation.clicked.connect(lambda: self.show_conversation("ether"))
         btnShowIPConversation.clicked.connect(lambda: self.show_conversation("ip"))
@@ -93,14 +93,15 @@ class NapMainWindow(QWidget):
         # Here we define the actions/methods/functions:
     def browse_files(self):
         response = QFileDialog.getOpenFileName(self, 'Open File', '.', 'PCAP files (*.pcap)')
-        if response[0] != "":	# to avoid crashing if the user cancels
+        if response[0] != "":  # to avoid crashing if the user cancels
             self.pathToFile.setText(response[0])
+
     def generate_conversations(self):
         pcap_file = self.pathToFile.text()
         NapMethods.generate_conversations(pcap_file)
-    
-    def show_conversation(self, mode = "off"):
-        self.napMode = mode # we update the mode of the just pressed button
+
+    def show_conversation(self, mode="off"):
+        self.napMode = mode  # we update the mode of the just pressed button
         if (mode == "ip"):
             twopi_figure = "./conversations/ip_conversation_twopi.gv.svg"
             dot_figure = "./conversations/ip_conversation_dot.gv.svg"
@@ -111,11 +112,12 @@ class NapMainWindow(QWidget):
             dot_figure = "./conversations/ethernet_conversation_dot.gv.svg"
             neato_figure = "./conversations/ethernet_conversation_neato.gv.svg"
             circo_figure = "./conversations/ethernet_conversation_circo.gv.svg"
-        if (self.napMode != "off"):		# IS NOT DETECTING WHETHER OR NOT YOU LOADED THE PCAP FILE
+        if (self.napMode != "off"):  # IS NOT DETECTING WHETHER OR NOT YOU LOADED THE PCAP FILE
             # 0.x the percentage (x0% of the QGraphicsView, 768 rendered width pixels of the svg images)
             scale_factor = 0.7 * self.view.width() / 768
-            if (not self.radioTwopi.isChecked() and not self.radioDot.isChecked() 
-                and not self.radioNeato.isChecked() and not self.radioCirco.isChecked()):
+            if (not self.radioTwopi.isChecked() and not self.radioDot.isChecked() and not
+                    self.radioNeato.isChecked() and not
+                    self.radioCirco.isChecked()):
                 svg_twopi = QGraphicsSvgItem(twopi_figure)
                 self.scene.addItem(svg_twopi)
                 svg_twopi.setScale(scale_factor)
@@ -141,15 +143,17 @@ class NapMainWindow(QWidget):
                     self.radioNeato.toggle()
                 elif (self.radioCirco.isChecked()):
                     self.scene.clear()
-                    svg_circo = QGraphicsSvgItem(circo_figure)  
+                    svg_circo = QGraphicsSvgItem(circo_figure)
                     self.scene.addItem(svg_circo)
                     svg_circo.setScale(scale_factor)
                     self.radioCirco.toggle()
+
 
 def runNap():
     app = QApplication(sys.argv)
     win = NapMainWindow()
     win.show()
     sys.exit(app.exec_())
+
 
 runNap()
